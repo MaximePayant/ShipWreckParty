cbefab1d8ba490177a697a7bdc6c9d6fc8a9fc2a
Maxime Payant <56399607+MaximePayant@users.noreply.github.com>
2022-02-25 13:59:49 +0100

    Initialize release

---
+++ Build/CMakeFiles/project.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/maxime/Repo_Tek/CreativeRift/template_linux/Project/sources/MainScene.cpp" "CMakeFiles/project.dir/Project/sources/MainScene.cpp.o" "gcc" "CMakeFiles/project.dir/Project/sources/MainScene.cpp.o.d"
+  "/home/maxime/Repo_Tek/CreativeRift/template_linux/Project/sources/ProjectModule.cpp" "CMakeFiles/project.dir/Project/sources/ProjectModule.cpp.o" "gcc" "CMakeFiles/project.dir/Project/sources/ProjectModule.cpp.o.d"
+  "/home/maxime/Repo_Tek/CreativeRift/template_linux/Project/sources/main.cpp" "CMakeFiles/project.dir/Project/sources/main.cpp.o" "gcc" "CMakeFiles/project.dir/Project/sources/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Build/CMakeFiles/project.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project.dir/Project/sources/MainScene.cpp.o"
+  "CMakeFiles/project.dir/Project/sources/MainScene.cpp.o.d"
+  "CMakeFiles/project.dir/Project/sources/ProjectModule.cpp.o"
+  "CMakeFiles/project.dir/Project/sources/ProjectModule.cpp.o.d"
+  "CMakeFiles/project.dir/Project/sources/main.cpp.o"
+  "CMakeFiles/project.dir/Project/sources/main.cpp.o.d"
+  "out/project"
+  "out/project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
